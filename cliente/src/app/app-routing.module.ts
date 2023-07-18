b122b38f9fb79c644073615b587c0910c2d28e4c
e-pasion <elian.pbh@gmail.com>
2023-07-18 11:14:21 -0500

    add validations

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './components/create-user/create-user.component';
-import { ReadUserComponent } from './components/read-user/read-user.component';
 
 const routes: Routes = [
 
-  {component:CreateUserComponent, path:""},
-  {component:ReadUserComponent, path:"read"}
+  {component:CreateUserComponent, path:""}
 ];
 
 @NgModule({
